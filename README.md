a224e8202087fdbda95eea7d631de982c8745849
Samuel Lando <samuel.lando@icloud.com>
2026-04-18 14:50:27 -0400

    Instal the scripts to the nix profile as well

--- README.md
+++ README.md
@@ -3,21 +3,21 @@
 This Project contains dot file configurations for my development environment 
 and my linux user environment.
 
-the devlopment environment uses nix, so that it can be deployed across differnet
+the development environment uses nix, so that it can be deployed across different
 OSes, and the user environment is currently just a set of dotfiles, so many things
 still need to be installed with the OS. Currently this is setup on ARCH linux.
 
 
 ## Development environment
 
-Run the bootstrapping script to setup the user home directory.
-```
-sh bootstrap.sh
-```
-
 Install all the required packages locally using NIX.
 ```
 nix profile install
+```
+
+Finally install the dotfiles into your home directory with the bootstrap script:
+```
+bootstrap_dots
 ```
 
 ## User environment
@@ -26,3 +26,4 @@ There is still some additional manual setup required for certain things like
 backups and gpg keys, but this is enough to get up and running quickly.
 
 1. Install the crontab from .crontab, this will enable automated backups
+
--- flake.nix
+++ flake.nix
@@ -1,91 +1,110 @@
 {
-    description = ''
-        Dev environment setup, meant to get everything up and running
-        quickly and consistently.
+  description = ''
+    Dev environment setup, meant to get everything up and running
+    quickly and consistently.
 
-        nvim + tmux
+    nvim + tmux
 
-        Trying to include all the tool needed. However the OS package manager
-        should be used to install other stuff, such as programming languages.
-    '';
+    Trying to include all the tool needed. However the OS package manager
+    should be used to install other stuff, such as programming languages.
+  '';
 
-    inputs = {
-        nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.11";
-    };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
 
-    outputs = {self, nixpkgs}:
-    let 
+  outputs = { self, nixpkgs }:
+    let
       systems = [ "x86_64-linux" "aarch64-linux" ];
       forAllSystems = nixpkgs.lib.genAttrs systems;
       nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
-      packagesFor = forAllSystems (system: with nixpkgsFor.${system}; [
-            fzf
-            tmux
-            curl
-            wget
-            ripgrep
-            unzip
-            fish
-            zsh
-            git
-            (neovim.override {
-              vimAlias = true;
-              viAlias = true;
-              configure = {
-                customLuaRC = ''
-                    require("sam")
-                    vim.g.gruvbox_material_enable_italic = true
-                    vim.cmd.colorscheme('gruvbox-material')
-                '';
-                packages.myVimPackage = with pkgs.vimPlugins; {
-                  start = [ 
-                    telescope-nvim
-                    codecompanion-nvim
-                    nvim-cmp
-                    cmp-nvim-lua
-                    cmp-nvim-lsp
-                    cmp-buffer
-                    cmp-path
-                    cmp-cmdline
-                    luasnip
-                    cmp_luasnip
-                    harpoon2
-                    oil-nvim
-                    lualine-nvim
-                    lsp-zero-nvim
-                    vim-sensible
-                    formatter-nvim
-                    indent-blankline-nvim
-                    mason-nvim
-                    nvim-lint
-                    nui-nvim
-                    nvim-treesitter.withAllGrammars
-                    mason-lspconfig-nvim
-                    gitsigns-nvim
-                    nvim-autopairs
-                    comment-nvim
-                    vim-fugitive
-                    rest-nvim
-                    undotree
-                    flash-nvim
-                    tabular
-                    diffview-nvim
-                    vim-repeat
-                    vim-surround
-                    gruvbox-material
-                    quickfix-reflector-vim
-                  ];
-                };
-              };
-            })
-          ]);
+      dotfilesFor = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
         in
-        {
-          packages = forAllSystems (system: {
-             default = nixpkgsFor.${system}.buildEnv {
-              name = "MyDevEnv";
-              paths = packagesFor.${system};
-             };
+        pkgs.stdenv.mkDerivation {
+          name = "dots";
+          src = self;
+          installPhase = ''
+            mkdir -p $out/share/home
+            cp $src/.zshenv $out/share/home/.zshenv
+            cp $src/.fishrc.fish $out/share/home/
+            cp $src/.crontab $out/share/home/
+            cp $src/.anacrontab $out/share/home/
+            cp -r $src/config $out/share/home/
+            mkdir -p $out/bin
+            cp $src/bootstrap_dots $out/bin/bootstrap_dots
+            cp $src/bin/* $out/bin
+          '';
         });
+      packagesFor = forAllSystems (system: with nixpkgsFor.${system}; [
+        fzf
+        tmux
+        curl
+        wget
+        ripgrep
+        unzip
+        fish
+        zsh
+        git
+        (neovim.override {
+          vimAlias = true;
+          viAlias = true;
+          configure = {
+            customLuaRC = ''
+              require("sam")
+              vim.g.gruvbox_material_enable_italic = true
+              vim.cmd.colorscheme('gruvbox-material')
+            '';
+            packages.myVimPackage = with pkgs.vimPlugins; {
+              start = [
+                telescope-nvim
+                codecompanion-nvim
+                nvim-cmp
+                cmp-nvim-lua
+                cmp-nvim-lsp
+                cmp-buffer
+                cmp-path
+                cmp-cmdline
+                luasnip
+                cmp_luasnip
+                harpoon2
+                oil-nvim
+                lualine-nvim
+                lsp-zero-nvim
+                vim-sensible
+                formatter-nvim
+                indent-blankline-nvim
+                mason-nvim
+                nvim-lint
+                nui-nvim
+                nvim-treesitter.withAllGrammars
+                mason-lspconfig-nvim
+                gitsigns-nvim
+                nvim-autopairs
+                comment-nvim
+                vim-fugitive
+                rest-nvim
+                undotree
+                flash-nvim
+                tabular
+                diffview-nvim
+                vim-repeat
+                vim-surround
+                gruvbox-material
+                quickfix-reflector-vim
+              ];
+            };
+          };
+        })
+      ]);
+    in
+    {
+      packages = forAllSystems (system: {
+        default = nixpkgsFor.${system}.buildEnv {
+          name = "MyDevEnv";
+          paths = packagesFor.${system} ++ [ dotfilesFor.${system} ];
+        };
+      });
     };
 }
